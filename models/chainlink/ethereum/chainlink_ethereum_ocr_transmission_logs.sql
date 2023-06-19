990a4140bf51da122592a63e868e7956104ee5a4
ryanostrom <rpostrom@gmail.com>
2023-06-19 15:30:31 -0500

    Refactor chainlink model; implement ethereum ocr gas models

---
+++ models/chainlink/ethereum/chainlink_ethereum_ocr_transmission_logs.sql
@@ -0,0 +1,28 @@
+{{ config(
+    alias = 'ocr_transmission_logs',
+    materialized = 'view',
+    post_hook='{{ expose_spells(\'["ethereum"]\',
+                                "sector",
+                                "chainlink",
+                                \'["linkpool_ryan"]\') }}'
+    )
+}}
+
+SELECT
+  'ethereum' as blockchain,
+  block_hash,
+  contract_address,
+  data,
+  topic0,
+  topic1,
+  topic2,
+  topic3,
+  tx_hash,
+  block_number,
+  block_time,
+  index,
+  tx_index
+FROM
+  ethereum.logs
+WHERE
+  topic0 = 0xf6a97944f31ea060dfde0566e4167c1a1082551e64b60ecb14d599a9d023d451
